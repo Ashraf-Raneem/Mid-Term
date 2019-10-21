113f27a56846144b9262f1af6d002773830be580
Ashraf-Raneem <you@example.com>
2019-10-20 20:44:18 -0700

    1st commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /* Add your script here */
+$("#border-width").click(function(){
+    var letter1 = $("#letter1").val().toLowerCase().css("font-size","32px");
+    var letter2 = $("#letter2").val().toLowerCase().css("font-size","18px");
+
+    $("#letter-container").append(letter1).append(letter2); 
+})
+
+
+
+$("#submitBtn").click(function(){
+    var borderWidth = $("#border-Width").val()
+
+    $("#letter-container").css("border-width","borderWidth").css("border-radius","2px");
+})
